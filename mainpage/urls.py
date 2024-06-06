0a9c8b971aeaecf1878c7b0767631c6b1b97ed92
totallynotnochi <105096693+totallynotnochi@users.noreply.github.com>
2024-06-05 22:13:11 -0400

    FORMs are fixed
    TODO:
    
    Redirected page has no stylesheets.

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mainpagereturn, name="MainPage"),
-    path('generic.html/', views.optimizerreturn, name="Optimizer")
+    path('', views.genshin_uid_input, name="MainPage"),
+    path('generic.html/', views.optimizerreturn, name="OptimizerAccess"),
+    path('dev/', views.optimizerreturn, name="DevPage"),
+    path('uid-loaded-index/', views.uid_loaded_index, name='uid_loaded_index')
     ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from mainpage.uidFORM import GenshinUidForm
+from django.shortcuts import render, redirect
 
 def mainpagereturn(request):
     return render(request, 'index.html')
@@ -11,17 +12,25 @@ def optimizerreturn(request):
 
 def genshin_uid_input(request):
     if request.method == 'POST':
+        print("POST SUCCESSS")
         form = GenshinUidForm(request.POST)
         if form.is_valid():
-            genshin_uid = form.cleaned_data['GenshinUID']
-            # Process the 'genshin_uid'
-            return HttpResponse("Success")
-        else:
-            error_message = "Invalid UID. Please enter a valid number."
-            return HttpResponse(error_message)
+            print("POST IS VALID?!?!!?")
+            uid = form.cleaned_data['GenshinUID']
+            request.session['uid'] = uid # Store UID in session
+            return redirect('uid_loaded_index')  # Redirect to new page
     else:
         form = GenshinUidForm()
     return render(request, 'index.html', {'form': form})
+
+def uid_loaded_index(request):
+    print("test")
+    uid = request.session.get('uid', None) # Retrieve UID from session
+    return render(request, 'UIDLoadedIndex.html', {'uid': uid})
+
+
+def HTMLdevreturn(request):
+    return render(request, 'am studying.html')
 
 
 
